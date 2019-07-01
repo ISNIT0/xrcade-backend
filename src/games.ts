357c51389d32297b58f2dc6dab6f3baf168deedd
Joe Reeve <joseph.reeve@googlemail.com>
2019-07-01 17:29:12 +0100

    :bento: update game asset paths

--- src/games.ts
+++ src/games.ts
@@ -1,16 +1,16 @@
 export const games = [{
     friendlyId: 'dance-tonite',
     title: 'Dance Tonite',
-    video: './videos/dance_tonite.mp4',
-    poster: 'https://supermedium.com/superassets/site/baristaexpress.jpg',
+    video: 'videos/dance_tonite.mp4',
+    poster: 'images/dance_tonite.png',
     description: 'Dance Tonite.',
     rating: 4.7,
     url: 'https://tonite.dance'
 }, {
     friendlyId: 'barista-express',
     title: 'Barista Express',
-    video: './videos/barista_express.mp4',
-    poster: 'https://supermedium.com/superassets/site/baristaexpress.jpg',
+    video: 'videos/barista_express.mp4',
+    poster: 'images/barista_express.png',
     description: 'Virtually be a barista in your own cafe.',
     rating: 4.7,
     url: 'https://constructarca.de/construct-arcade/game/barista-express/game/'
