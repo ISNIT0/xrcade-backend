b0800b1818b5dac2570e5c0c96afc8238b623d5c
Joe Reeve <joseph.reeve@googlemail.com>
2019-07-04 11:37:58 +0100

    Fix typo :)

--- src/games.ts
+++ src/games.ts
@@ -4,124 +4,124 @@ export const games = [{
     video: 'videos/dance_tonite.mp4',
     poster: 'images/dance_tonite.png',
     description: 'Dance Tonite.',
-    url: 'https://tonite.dance'
+    url: 'https://tonite.dance',
 }, {
     friendlyId: 'barista-express',
     title: 'Barista Express',
     video: 'videos/barista_express.mp4',
     poster: 'images/barista_express.png',
     description: 'Virtually be a barista in your own cafe.',
-    url: 'https://constructarca.de/construct-arcade/game/barista-express/game/'
+    url: 'https://constructarca.de/construct-arcade/game/barista-express/game/',
 }, {
     friendlyId: 'pain-run',
     title: 'Pain Run',
     video: 'videos/pain_run.mp4',
-    //poster: 'images/.png',
+    // poster: 'images/.png',
     description: 'Avoid the obstacles in the tunnel as they come towards you',
-    url: 'https://gfwilliams.github.io/PainRun/'
+    url: 'https://gfwilliams.github.io/PainRun/',
 }, {
     friendlyId: 'moon-rider',
     title: 'Moon Rider',
     video: 'videos/moon_rider.mp4',
-    //poster: 'images/.png',
+    // poster: 'images/.png',
     description: 'Surf the musical road among the stars, moon, and lights',
-    url: 'https://moonrider.xyz/'
+    url: 'https://moonrider.xyz/',
 }, {
     friendlyId: 'super-says',
     title: 'Super Says',
     video: 'videos/super_says.mp4',
-    //poster: 'images/.png',
+    // poster: 'images/.png',
     description: 'Test your memory with Super. Memorize the color sequence and grab a high score!',
-    url: 'https://supermedium.com/supersays/src/'
+    url: 'https://supermedium.com/supersays/src/',
 }, {
     friendlyId: 'soundboxing',
     title: 'Soundboxing',
     video: 'videos/.mp4',
-    //poster: 'images/.png',
-    description: 'A VR music kickboxing game with beats created by people. If you feel the urge to dance, you're doing it right!',
-    url: 'https://webvr.soundboxing.co/'
+    // poster: 'images/.png',
+    description: `A VR music kickboxing game with beats created by people. If you feel the urge to dance, you're doing it right!`,
+    url: 'https://webvr.soundboxing.co/',
 }, {
     friendlyId: 'make-it-rain',
     title: 'Make It Rain',
     video: 'videos/make_it_rain.mp4',
-    //poster: 'images/.png',
+    // poster: 'images/.png',
     description: 'Make it Rain VR puts you into the centre of a monsoon of cash. Relax and let the dollars pour down onto you',
-    url: 'https://webvr-makeitrain.glitch.me/'
+    url: 'https://webvr-makeitrain.glitch.me/',
 }, {
     friendlyId: 'mozilla-hubs',
     title: 'hubs by Mozilla',
     video: 'videos/mozilla_hubs.mp4.mp4',
-    //poster: 'images/.png',
+    // poster: 'images/.png',
     description: 'Share a virtual room with friends. Watch videos, play with 3D objects, or just hang out',
-    url: 'https://hubs.mozilla.com/'
+    url: 'https://hubs.mozilla.com/',
 }, {
     friendlyId: 'trajectile-command',
     title: 'Trajectile Command',
     video: 'videos/trajectile_command.mp4',
-    //poster: 'images/.png',
+    // poster: 'images/.png',
     description: 'An ar­cade style game in which you de­fend your cities from waves of en­e­my bombers and mis­siles by in­struct­ing your own mis­sile launch­ers where to fire',
-    url: 'http://micosmo.com/trajectilecommand/'
+    url: 'http://micosmo.com/trajectilecommand/',
 }, {
     friendlyId: 'vuppets',
     title: 'Vuppets',
     video: 'videos/vuppets.mp4',
-    //poster: 'images/.png',
+    // poster: 'images/.png',
     description: 'Put on a show with these cute animated hand puppets',
-    url: 'https://www.jorgefuentes.net/projects/vuppets/vuppets_6DOF/'
+    url: 'https://www.jorgefuentes.net/projects/vuppets/vuppets_6DOF/',
 }, {
     friendlyId: 'blue-cyber-e1',
     title: 'Blue Cyber: Episode 1',
     video: 'videos/blue_cyber.mp4',
-    //poster: 'images/.png',
+    // poster: 'images/.png',
     description: 'You are charged to protect the galaxy with powers given from the artfiact, Blue Cyber. A science fantasy VR story',
-    url: 'https://www.bluecybervr.com/episode01/'
+    url: 'https://www.bluecybervr.com/episode01/',
 }, {
     friendlyId: 'monster-or-friend',
     title: 'Monster or Friend',
     video: 'videos/monster_or_friend.mp4',
-    //poster: 'images/.png',
+    // poster: 'images/.png',
     description: 'You are followed by a procedurally animated sea space creature. A visual and audio experience',
-    url: 'https://codercat.tk/monster-or-friend/'
+    url: 'https://codercat.tk/monster-or-friend/',
 }, {
     friendlyId: 'under-neon-lights',
     title: 'Under Neon Lights',
     video: 'videos/under_neon_lights.mp4',
-    //poster: 'images/.png',
+    // poster: 'images/.png',
     description: 'Enter a world of imagination and color in this interactive music experience for The Chemical Brothers Under Neon Lights',
-    url: 'https://www.with.in/watch/under-neon-lights/'
+    url: 'https://www.with.in/watch/under-neon-lights/',
 }, {
     friendlyId: 'sabre-trainer',
     title: 'Sabre Trainer',
     video: 'videos/sabre_trainer.mp4',
-    //poster: 'images/.png',
-    description: 'Beat Sabre style game gradually increases the frequency of the blocks. If you start missing, it'll slow down and gradually speed up again as you start getting hits',
-    url: 'https://wwwtyro.net/games/saber-trainer/'
+    // poster: 'images/.png',
+    description: `Beat Sabre style game gradually increases the frequency of the blocks. If you start missing, it'll slow down and gradually speed up again as you start getting hits`,
+    url: 'https://wwwtyro.net/games/saber-trainer/',
 }, {
     friendlyId: 'the-hall',
     title: 'The Hall',
     video: 'videos/the_hall.mp4',
-    //poster: 'images/.png',
+    // poster: 'images/.png',
     description: 'A fine little museum. Statues, kinectic sculptures, and paintings',
-    url: 'https://cecropia.github.io/thehallaframe/'
+    url: 'https://cecropia.github.io/thehallaframe/',
 }, {
     friendlyId: 'supercraft',
     title: 'Supercraft',
     video: 'videos/supercraft.mp4',
-    //poster: 'images/.png',
+    // poster: 'images/.png',
     description: 'Build a VR world in minutes! Publish your creation instantly to the Web to share with your friends',
-    url: 'https://supermedium.com/craft/'
+    url: 'https://supermedium.com/craft/',
 }, {
     friendlyId: 'a-painter',
     title: 'A-Painter',
     video: 'videos/a_painter.mp4',
-    //poster: 'images/.png',
+    // poster: 'images/.png',
     description: 'Paint in VR! Create a masterpiece with dozens of brushes available. Save your painting and share it with your friends',
-    url: 'https://aframe.io/a-painter/'
+    url: 'https://aframe.io/a-painter/',
 }, {
     friendlyId: 'realms-vr',
     title: 'Realms VR',
     video: 'videos/realms_vr.mp4',
-    //poster: 'images/.png',
+    // poster: 'images/.png',
     description: 'Recursive VR voxel viewer and editor',
-    url: 'https://realmsvr.gatunes.com/'
+    url: 'https://realmsvr.gatunes.com/',
 }];
