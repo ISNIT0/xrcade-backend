c2729636ebf19e8fa2b4d49539d7dee04d6d21e9
Gordon Williams <gw@pur3.co.uk>
2019-07-04 11:18:36 +0100

    New games linked, no descriptions/tags yet

--- src/games.ts
+++ src/games.ts
@@ -12,4 +12,116 @@ export const games = [{
     poster: 'images/barista_express.png',
     description: 'Virtually be a barista in your own cafe.',
     url: 'https://constructarca.de/construct-arcade/game/barista-express/game/'
+}, {
+    friendlyId: 'pain-run',
+    title: 'Pain Run',
+    video: 'videos/pain_run.mp4',
+    //poster: 'images/.png',
+    description: '',
+    url: 'https://gfwilliams.github.io/PainRun/'
+}, {
+    friendlyId: 'moon-rider',
+    title: 'Moon Rider',
+    video: 'videos/moon_rider.mp4',
+    //poster: 'images/.png',
+    description: '',
+    url: 'https://moonrider.xyz/'
+}, {
+    friendlyId: 'super-says',
+    title: 'Super Says',
+    video: 'videos/super_says.mp4',
+    //poster: 'images/.png',
+    description: '',
+    url: 'https://supermedium.com/supersays/src/'
+}, {
+    friendlyId: 'soundboxing',
+    title: 'Soundboxing',
+    video: 'videos/.mp4',
+    //poster: 'images/.png',
+    description: '',
+    url: 'https://webvr.soundboxing.co/'
+}, {
+    friendlyId: 'make-it-rain',
+    title: 'Make It Rain',
+    video: 'videos/make_it_rain.mp4',
+    //poster: 'images/.png',
+    description: '',
+    url: 'https://webvr-makeitrain.glitch.me/'
+}, {
+    friendlyId: 'mozilla-hubs',
+    title: 'hubs by Mozilla',
+    video: 'videos/mozilla_hubs.mp4.mp4',
+    //poster: 'images/.png',
+    description: '',
+    url: 'https://hubs.mozilla.com/'
+}, {
+    friendlyId: 'trajectile-command',
+    title: 'Trajectile Command',
+    video: 'videos/trajectile_command.mp4',
+    //poster: 'images/.png',
+    description: '',
+    url: 'http://micosmo.com/trajectilecommand/'
+}, {
+    friendlyId: 'vuppets',
+    title: 'Vuppets',
+    video: 'videos/vuppets.mp4',
+    //poster: 'images/.png',
+    description: '',
+    url: 'https://www.jorgefuentes.net/projects/vuppets/vuppets_6DOF/'
+}, {
+    friendlyId: 'blue-cyber',
+    title: 'Blue Cyber',
+    video: 'videos/blue_cyber.mp4',
+    //poster: 'images/.png',
+    description: '',
+    url: 'https://www.bluecybervr.com/episode01/'
+}, {
+    friendlyId: 'monster-or-friend',
+    title: 'Monster or Friend',
+    video: 'videos/monster_or_friend.mp4',
+    //poster: 'images/.png',
+    description: '',
+    url: 'https://codercat.tk/monster-or-friend/'
+}, {
+    friendlyId: 'under-neon-lights',
+    title: 'Under Neon Lights',
+    video: 'videos/under_neon_lights.mp4',
+    //poster: 'images/.png',
+    description: '',
+    url: 'https://www.with.in/watch/under-neon-lights/'
+}, {
+    friendlyId: 'sabre-trainer',
+    title: 'Sabre Trainer',
+    video: 'videos/sabre_trainer.mp4',
+    //poster: 'images/.png',
+    description: '',
+    url: 'https://wwwtyro.net/games/saber-trainer/'
+}, {
+    friendlyId: 'the-hall',
+    title: 'The Hall',
+    video: 'videos/the_hall.mp4',
+    //poster: 'images/.png',
+    description: '',
+    url: 'https://cecropia.github.io/thehallaframe/'
+}, {
+    friendlyId: 'supercraft',
+    title: 'Supercraft',
+    video: 'videos/supercraft.mp4',
+    //poster: 'images/.png',
+    description: '',
+    url: 'https://supermedium.com/craft/'
+}, {
+    friendlyId: 'a-painter',
+    title: 'A-Painter',
+    video: 'videos/a_painter.mp4',
+    //poster: 'images/.png',
+    description: '',
+    url: 'https://aframe.io/a-painter/'
+}, {
+    friendlyId: 'realms-vr',
+    title: '',
+    video: 'videos/realms_vr.mp4',
+    //poster: 'images/.png',
+    description: '',
+    url: 'https://realmsvr.gatunes.com/'
 }];
